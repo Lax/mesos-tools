9ec502a95bed4d123eba1b1b6807f18d5ed2910c
Liu Lantao <liulantao@gmail.com>
2016-09-29 18:49:19 +0800

    build vamp & vamp UI

---
+++ build/build_vamp.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# Usage:
+#   docker run -i -t --rm -v `pwd`:/build -w /build --entrypoint /build/build_vamp.sh centos:7
+
+function Usage () {
+    echo "Usage:"
+    echo "  $0 run"
+}
+
+if [ $# -eq 0 ]
+then
+    Usage
+    exit
+fi
+
+if [ $# -eq 1 -a "$1" = "run" ]
+then
+   docker run -i -t --rm -v `pwd`:/build -w /build --entrypoint /build/build_vamp.sh centos:7 build
+   exit
+fi
+
+if [ $# -eq 1 -a "$1" = "build" ]
+then
+    cd $(dirname $0)
+
+    yum install -q -y git
+    git clone https://github.com/magneticio/vamp.git
+    cd vamp
+
+    curl -s https://bintray.com/sbt/rpm/rpm -o /etc/yum.repos.d/sbt.repo
+    yum install -q -y sbt
+
+    sbt test assembly
+
+    cp $(find bootstrap/target/scala-* -name 'vamp-assembly-*.jar' | sort | tail -1) vamp.jar
+fi
---
+++ build/build_vamp_ui.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# Usage:
+#   docker run -i -t --rm -v $(pwd):/build -w /build --entrypoint /build/build_vamp_ui.sh node:6
+
+function Usage () {
+    echo "Usage:"
+    echo "  $0 run"
+}
+
+if [ $# -eq 0 ]
+then
+    Usage
+    exit
+fi
+
+if [ $# -eq 1 -a "$1" = "run" ]
+then
+    docker run -i -t --rm -v $(pwd):/build -w /build --entrypoint /build/build_vamp_ui.sh node:6 build
+    exit
+fi
+
+if [ $# -eq 1 -a "$1" = "build" ]
+then
+    cd $(dirname $0)
+
+    git clone https://github.com/magneticio/vamp-ui.git
+    cd vamp-ui
+
+    npm install -g bower "gulpjs/gulp#4.0"
+
+    npm install && bower install --allow-root && ./setEnvironment.sh && gulp build
+
+    mv dist ui && tar -cvjSf ui.tar.bz2 ui
+fi
